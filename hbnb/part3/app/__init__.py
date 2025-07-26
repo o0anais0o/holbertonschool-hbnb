4a90026d4fe41770267853893ffa3879fc598506
o0anais0o <choisy_anais@hotmail.fr>
2025-07-26 18:24:41 +0200

    Maj __init__.py

--- hbnb/part3/app/__init__.py
+++ hbnb/part3/app/__init__.py
@@ -82,7 +82,7 @@ def create_app(config_name='default'):
     from app.api.v1.places import api as places_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.reviews import api as reviews_ns
-    from app.api.v1.auth import auth_ns, auth_bp, web_bp
+    from app.api.v1.auth import api as auth_ns, auth_bp, web_bp
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
